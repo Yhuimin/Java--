b021d2faa4ed22fe3d05f36b591f81c828d6846e
Yhuimin <72692026+Yhuimin@users.noreply.github.com>
2020-10-26 19:03:53 +0800

    Update Student.java

--- Student.java
+++ Student.java
@@ -6,24 +6,25 @@ public class Student extends Person{
 	//choosecourse不是Student的属性
 	private Course choosecourse;
 	
+	//无参构造方法
 	public Student() {
 		super();
 		// TODO Auto-generated constructor stub
 	}	
-			
+	//有参构造方法		
 	public Student(int id1, String name1, String sex1) {
 		super(id1, name1, sex1);
 		// TODO Auto-generated constructor stub
 	}
-
+	//调用get方法获取选课信息
 	public Course getChoosecourse() {
 		return choosecourse;
 	}
-
+	//调用set方法设置选课信息
 	public void setChoosecourse(Course choosecourse) {
 		this.choosecourse = choosecourse;
 	}
-
+       //打印输出学生信息
 	@Override
 	public String toString() {
 		return super.toString()+"Student [choosecourse="+ choosecourse + "]";
